687f275d7e76ea82f4b14844e439171cbb9a895a
CeydaSerttas <cceydasert@gmail.com>
2025-07-31 00:38:00 +0300

    add app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center" }}>
+      <h1>🌸 Fairycore CV Builder</h1>
+      <p>Hoş geldin! Bu bir deneme çalışmasıdır.</p>
+    </div>
+  );
+}
